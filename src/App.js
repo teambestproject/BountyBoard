1f2cad8cc6b82e57ecc0132b82027785040cf2ab
Skarrg <orestes.moiran@gmail.com>
2019-02-24 18:15:13 -0800

    Bounty rendering subchildren inside of it. Uncomplicated the file structure for Bounty Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bounty from './Components/Bounty';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Bounty>
+        <Bounty.BountyTitle></Bounty.BountyTitle>
+        <Bounty.BountyInfo></Bounty.BountyInfo>
+        <Bounty.BountyRewards></Bounty.BountyRewards>
+      </Bounty>
     );
   }
 }
