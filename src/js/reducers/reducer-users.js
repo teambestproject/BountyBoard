6e23885f33a0f6b03c4dd6e8addc210a9223675e
nBow <nbow51@gmail.com>
2019-02-25 19:09:08 -0800

    save

---
+++ src/js/reducers/reducer-users.js
@@ -0,0 +1,41 @@
+
+export default function(); {
+    return [
+        {
+            id: 1,
+            displayName: "nBow",
+            first: "Casey",
+            last: "Shore",
+            age: 26,
+            description: "Loves to play video games",
+            thumbnail: "https://avatars0.githubusercontent.com/u/860659?s=460&v=4"
+        },
+        {
+            id: 2,
+            displayName: "Skarrg",
+            first: "Justin",
+            last: "Honegger",
+            age: 26,
+            description: "Loves to make video games",
+            thumbnail: "https://avatars2.githubusercontent.com/u/8120174?s=460&v=4"
+        },
+        {
+            id: 3,
+            displayName: "hutwagnert",
+            first: "Tate",
+            last: "Hutwagner",
+            age: 26,
+            description: "Loves to record the data of videogames",
+            thumbnail: "https://avatars1.githubusercontent.com/u/42015610?s=460&v=4"
+        },
+        {
+            id: 4,
+            displayName: "gremman",
+            first: "Glory",
+            last: "Avina",
+            age: 26,
+            description: "Loves to win in video games",
+            thumbnail: "https://avatars0.githubusercontent.com/u/37358738?s=460&v=4"
+        },
+    ]
+}
