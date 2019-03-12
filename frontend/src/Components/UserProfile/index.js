ee02dfd1e260761c09c10f6cadc761c5d873a7d0
Skarrg <orestes.moiran@gmail.com>
2019-03-11 21:01:08 -0700

    classname

--- frontend/src/Components/UserProfile/index.js
+++ frontend/src/Components/UserProfile/index.js
@@ -5,30 +5,30 @@ import "./style.css";
 
 const UserProfile = () => (
     <div className="userprofile">
-        <div class="column">
+        <div className="column">
         <h3> My Info </h3>
         <br />
-            <ul class="list">
+            <ul className="list">
                 <li> Years of experience: <input type="text" id="text"/><input type="button" value="Edit" id="button"/>​</li>
                 <li> Areas of expertise: <input type="text" id="text"/><input type="button" value="Edit" id="button"/>​</li>
                 <li> Academic degree: <input type="text" id="text"/><input type="button" value="Edit" id="button"/>​</li>
             </ul>            
         </div>
         <br />
-        <div class="column">
+        <div className="column">
         <h3> Projects </h3>
         <br />
-            <ul class="list">
+            <ul className="list">
                 <li> Project 1: <input type="text" id="text"/>​</li>
                 <li> Project 2: <input type="text" id="text"/>​</li>
                 <li> Project 3: <input type="text" id="text"/>​</li>
             </ul>
         </div>
         <br />
-        <div class="column">
+        <div className="column">
         <h3> Rewards </h3>
         <br />
-            <ul class="list">
+            <ul className="list">
                 <li> Points Earned: <input type="text" id="text"/>​</li>
                 <li> Projects Completed: <input type="text" id="text"/></li>
                 <li> Performance Score: <input type="text" id="text"/>​</li>
