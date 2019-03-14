366b29f9ad24e64c272f143cff8e22da655ff84d
Skarrg <orestes.moiran@gmail.com>
2019-03-13 18:56:36 -0700

    adding in all links

--- frontend/src/Components/Dropdown/index.js
+++ frontend/src/Components/Dropdown/index.js
@@ -33,8 +33,11 @@ render() {
         <div className="button" onClick={this.showDropdownMenu}> Pages+ </div>
           { this.state.displayMenu ? (
             <ul class="pages">
-                <li><a href="/bounties">Bounties</a></li>
+                <li><a href="/bounties">Open Bounties</a></li>
                 <li><a href="/user">User Profile</a></li>
+                <li><a href="/create">Create a Bounty</a></li>
+                <li><a href="/mybounties">My Bounties</a></li>
+                <li><a href="/welcome">Home</a></li>
             </ul>
             ):
         (null)
