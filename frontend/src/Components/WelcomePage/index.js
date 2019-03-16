baceea08f9f345b42b7256643454a125f3fb83aa
Skarrg <orestes.moiran@gmail.com>
2019-03-16 10:11:22 -0700

    Responsive welcome page

--- frontend/src/Components/WelcomePage/index.js
+++ frontend/src/Components/WelcomePage/index.js
@@ -2,17 +2,16 @@ import React from "react";
 import "./style.css";
 
 
-function WelcomePage () {
+function WelcomePage() {
     return (
-    <div className="welcomeTitle1">
-        <div className="welcomeTitle">
-            <h2> WELCOME </h2>
+        <div className="welcomeTitle1">
+            <div className="welcomeTitle">
+                <h2> WELCOME </h2>
+            </div>
+            <div className="about">
+                <p> Bounty Bonus allows you to create projects and reward the team members who contribute to them. It also allows you to join teams and earn rewards as you work on projects. List your areas of expertise. Join projects. See your performance and collect rewards!</p>
+            </div>
         </div>
-        <div className="about">
-
-            <p> Bounty Bonus allows you to create projects and reward the team members who contribute to them. It also allows you to join teams and earn rewards as you work on projects. List your areas of expertise. Join projects. See your performance and collect rewards!</p>     
-        </div>
-    </div>
     );
 }
 
