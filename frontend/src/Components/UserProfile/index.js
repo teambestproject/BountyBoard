4e9fb0b8ea30daceb52b34247b1607e77d09981b
Skarrg <orestes.moiran@gmail.com>
2019-03-16 11:12:51 -0700

    upgrade

--- frontend/src/Components/UserProfile/index.js
+++ frontend/src/Components/UserProfile/index.js
@@ -1,41 +1,38 @@
 import React from "react";
 import "./style.css";
+import firebase from "firebase";
 
 //Main component for User Profile page. Belongs in main display
 
 const UserProfile = () => (
     <div className="userprofile">
 
-        <div className="column">
-        <h3> My Info </h3>
-        <br />
-            <ul className="list">
-                <li> Years of experience: <input type="text" id="text"/><input type="button" value="Edit" id="button"/>​</li>
-                <li> Areas of expertise: <input type="text" id="text"/><input type="button" value="Edit" id="button"/>​</li>
-                <li> Academic degree: <input type="text" id="text"/><input type="button" value="Edit" id="button"/>​</li>
-            </ul>            
+    <img className="profpic" src={firebase.auth().currentUser.photoURL} alt="profile"/>
+
+        <div className="column info">
+            <h3> My Info </h3>
+            <br />
+            <div> Years of experience: <input type="text" id="text" /><input type="button" value="Edit" id="button" />​</div>
+            <div> Areas of expertise:  <input type="text" id="text" /><input type="button" value="Edit" id="button" />​</div>
+            <div> Academic degree:     <input type="text" id="text" /><input type="button" value="Edit" id="button" />​</div>
         </div>
         <br />
 
-        <div className="column">
-        <h3> Projects </h3>
-        <br />
-            <ul className="list">
-                <li> Project 1: <input type="text" id="text"/>​</li>
-                <li> Project 2: <input type="text" id="text"/>​</li>
-                <li> Project 3: <input type="text" id="text"/>​</li>
-            </ul>
+        <div className="column projects">
+            <h3> Projects </h3>
+            <br />
+            <div> Project 1: <input type="text" id="text" />​</div>
+            <div> Project 2: <input type="text" id="text" />​</div>
+            <div> Project 3: <input type="text" id="text" />​</div>
         </div>
         <br />
 
-        <div className="column">
-        <h3> Rewards </h3>
-        <br />
-            <ul className="list">
-                <li> Points Earned: <input type="text" id="text"/>​</li>
-                <li> Projects Completed: <input type="text" id="text"/></li>
-                <li> Performance Score: <input type="text" id="text"/>​</li>
-            </ul>
+        <div className="column rewards">
+            <h3> Rewards </h3>
+            <br />
+            <div> Points Earned:      <input type="text" id="text" />​</div>
+            <div> Projects Completed: <input type="text" id="text" /></div>
+            <div> Performance Score:  <input type="text" id="text" />​</div>
         </div>
     </div>
 );
